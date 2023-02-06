a092035e7fa7f65e1747d9f5132c12ad29ccccaf
dasoib <dasojib49@gmail.comm>
2023-02-07 01:45:23 +0600

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-function mindGame(number){
-          if(typeof number != "number" || typeof number == "string" || number < 0){
-                    return "Please insert a valid value"
-          }
-          let result =((number*3+10)/2)-5;
-          return result;
-}
+// first description:This is a simple mind game.In this game we have to take an user input than multiply by three and add ten with the result than devide by two and the devision result subtracted by five.
+// function mindGame(number){
+//           if(typeof number != "number" || typeof number == "string" || number < 0){
+//                     return "Please insert a valid value"
+//           }
+//           let result =((number*3+10)/2)-5;
+//           return result;
+// }
 
-let final= mindGame(-5);
-console.log(final);
+
+// Second Description:In this problem i have to find the length of the string .Than devide the length by two to check it is even if it is not devided by two it will be evenOdd.
+// function evenOdd(string) {
+//           for (let i = string.length; i >= 0; i--) {
+//                     if (i % 2 == 0) {
+//                               return "Even"
+//                     } else {
+//                               return "Odd"
+//                     }
+//           }
+//           return i;
+// }
