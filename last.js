cc02cfcce7cb9426dc90226462bb31d01c8f5321
dasoib <dasojib49@gmail.comm>
2023-02-07 12:04:04 +0600

    sixth commit

--- last.js
+++ last.js
@@ -3,11 +3,15 @@ function gemsToDiamond(firstFriendGems,secondFriendGems,thirdFriendGems){
           let secondGemsPower = secondFriendGems*32;
           let thirdGemsPower = thirdFriendGems*43;
           let totalDiamond = firstGemsPower + secondGemsPower + thirdGemsPower;
+          let finalDiamond ='';
           if(totalDiamond > (1000*2)){
-                    let finalDiamond = totalDiamond - 2000;
-                    console.log(finalDiamond);
+                    finalDiamond = totalDiamond - 2000;
           }
+          else{
+                    console.log(totalDiamond);
+          }
+          console.log(finalDiamond);
 }
 
-let what = gemsToDiamond(1,1,1);
-console.log(what);
+gemsToDiamond(100,5,1);
+// console.log(what);
